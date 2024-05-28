8f5267ea09fe16a889e7267fb606d0acc567c393
claudiney63 <claudineyryan75@gmail.com>
2024-05-28 06:25:08 -0300

    feat: Add input validation to Contador class

--- module-2-dominando-java/desafios/DesafioControleFluxo/src/Contador.java
+++ module-2-dominando-java/desafios/DesafioControleFluxo/src/Contador.java
@@ -9,16 +9,26 @@ public class Contador {
         System.out.print("Digite o primeiro numero: ");
         x = scanner.nextInt();
         System.out.print("Digite o segundo numero: ");
-        y = scanner.nextInt();   
+        y = scanner.nextInt(); 
+        
+        try {
+            contar(x, y);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
 		
 		int contagem = parametroDois - parametroUm;
+
 		//realizar o for para imprimir os números com base na variável contagem
         for(int i = 0; i <= contagem; i++) {
-            System.out.println("Imprimindo o  numero " + i++);
+            System.out.println("Imprimindo o  numero " + i);
         }
 	}
 }
