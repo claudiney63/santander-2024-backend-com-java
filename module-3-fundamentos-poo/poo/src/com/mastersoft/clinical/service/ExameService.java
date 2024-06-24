17cd88abb35fc3a86800a7adc10c03991045d434
claudiney63 <claudineyryan75@gmail.com>
2024-06-24 01:25:58 -0300

    feat: fundamentos de pacotes em poo

---
+++ module-3-fundamentos-poo/poo/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,9 @@
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+    void salvarExame(Exame exame) {
+        System.out.println("Exame cadastrado com sucesso!");
+    }
+}
